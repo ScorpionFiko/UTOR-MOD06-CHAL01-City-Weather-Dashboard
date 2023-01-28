36ad903ba79d2b919c675252ebd69508ded5bb88
Stefan Marinov <stefan_marinov@rogers.com>
2023-01-28 17:56:28 -0500

    completed city search functionality accounting for multiple cities with same name; created API js to 'hide' the API key

---
+++ assets/javascript/apiKey.js
@@ -0,0 +1,3 @@
+export function openWeatherApiKey() {
+    return "a3af393ac27598dbf7a80e53c857e4ab";
+}
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,12 @@
+import { openWeatherApiKey } from "./apiKey.js"; 
+
+let apiKey = openWeatherApiKey();
 let storedSearchHistory = [];
 let localStorageSearchHistoryName = "searchHistory";
+let cityLat = 0;
+let cityLon = 0;
+let cityState ="";
+let cityCountry = "";
 
 function loadStoredSearchHistory() {
     storedSearchHistory = [];
@@ -11,11 +18,15 @@ function loadStoredSearchHistory() {
 function populateSearchHistory() {
     storedSearchHistory.forEach((storedSearchItem, index) => {
         $('#selectSearchHistory').append($('<option/>', {
-            text: storedSearchItem,
-            value: storedSearchItem,
-            id: "searchHistory-" + index
+            text: getCityStateCountry(storedSearchItem),
+            id: storedSearchItem.id
         }));
-        $('#searchHistory-' + index).attr("data-city", storedSearchItem);
+        $('#' + storedSearchItem.id).attr("data-id", storedSearchItem.id);
+        $('#' + storedSearchItem.id).attr("data-name", storedSearchItem.name);
+        $('#' + storedSearchItem.id).attr("data-state", storedSearchItem.state);
+        $('#' + storedSearchItem.id).attr("data-country", storedSearchItem.country);
+        $('#' + storedSearchItem.id).attr("data-lat", storedSearchItem.lat);
+        $('#' + storedSearchItem.id).attr("data-lon", storedSearchItem.lon);
     });
 }
 
@@ -29,12 +40,22 @@ function clearSearchHistory() {
         value: 0,
         id: "searchHistory-item"
     }));
-    $('#searchHistory-item').attr("data-city", "");
+    $('#searchHistory-item').attr("data-name", "");
 }
 
-function saveSearchHistory(searchCity) {
-    if (storedSearchHistory.findIndex(city => city === searchCity) < 0 ) {
-        storedSearchHistory.push(searchCity);
+function saveSearchHistory(cityData) {
+    // geting unique ID based on city and coordinates
+    let cityID = getCityId(cityData);
+
+    if (storedSearchHistory.findIndex(city => city.id === cityID) < 0 ) {
+        storedSearchHistory.push({
+            id: cityID,
+            name: cityData.name,
+            lat: cityData.lat,
+            lon: cityData.lon,
+            state: cityData.state,
+            country: cityData.country
+        });
         localStorage.setItem(localStorageSearchHistoryName, JSON.stringify(storedSearchHistory));
     }
 }
@@ -46,31 +67,144 @@ function setupSearchHistory() {
     populateSearchHistory();
 }
 
-function setupWeatherData(inputCity) {
-    console.log("setting up weather data for " + inputCity);
+function setupWeatherData(cityData) {
+    getWeatherData(cityData)
+}
+
+
+function getWeatherData(cityData) {
+    let weatherAPI = "https://api.openweathermap.org/data/2.5/forecast?lat=" + cityData.lat + "&lon=" + cityData.lon + "&appid=" + openWeatherApiKey() + "&units=metric";
+
+    fetch(weatherAPI).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (weatherData) {
+                console.log(weatherData);
+            });
+        }
+    });
+}
+
+function renderWeatherData() {
+
+}
+
+function displayCitiesWithSameName(inputCity) {
+    // get the coordinates
+    getCityCoordinates(inputCity);
+    
+}
+
+function getCityCoordinates(inputCity) {
+    let coordinatesApi = "http://api.openweathermap.org/geo/1.0/direct?q=" + inputCity + "&appid=" + openWeatherApiKey() + "&limit=5";
+    fetch(coordinatesApi).then(function(response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+              if (data.length > 1) {
+                createCityList(data);
+                $("#cityList").modal("show");
+              }  else if (data.length === 1) {
+                $("#citySearch").modal("hide");
+                $("#inputCitySearch").val("");
+                saveSearchHistory(data[0]);
+                setupSearchHistory();
+                setupWeatherData(data[0]);
+              } else {
+                alert("City named `" + inputCity + "` is not found!  Please try again!")
+              }
+            });
+        }
+    });
+
+}
+
+function getCityStateCountry(city) {
+    return city.name + ", " + ((city.state != undefined) ? city.state + ", " : "")  + city.country
+}
+
+function getCityId(city) {
+    return btoa(
+        city.name + city.state + city.country + city.lat + city.lon
+    ).replace(/=/g,"");
+}
+
+function createCityList(cityList) {
+    $('#cityListSelector').text('');
+    cityList.forEach((city, index ) => {
+        let cityID = getCityId(city);
+        $('#cityListSelector').append($('<a/>', {
+                text: getCityStateCountry(city),
+                id: cityID,
+                href:"#"
+            }));
+            $('#' + cityID).attr("data-id", cityID);
+            $('#' + cityID).attr("data-name", city.name);
+            $('#' + cityID).attr("data-lat", city.lat);
+            $('#' + cityID).attr("data-lon", city.lon);
+            $('#' + cityID).attr("data-state", city.state);
+            $('#' + cityID).attr("data-country", city.country);
+            $('#' + cityID).addClass("list-group-item-action");
+    });
+
 }
 
 $(function() {
     setupSearchHistory();
 
 
-    $("#btnSearch").on("click", function() {
+    $("#btnSearch").on("click", function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
         let searchCity = $("#inputCitySearch").val().trim();
         if (searchCity !== "") {
-            saveSearchHistory(searchCity);
-            setupSearchHistory();
-            setupWeatherData(searchCity);
+            displayCitiesWithSameName(searchCity);
+            
     
         } else {
             alert("Please enter a city");
         }
     });
 
-    $("#selectSearchHistory").on("change" , function(){
-        let historyCity = $("#selectSearchHistory :selected").attr("data-city");
-        if (historyCity !== "") {
+    $("#selectSearchHistory").on("change" , function(event){
+        event.preventDefault();
+        event.stopPropagation();
+        let selectedEl = $("#selectSearchHistory :selected");
+        let historyCity = {
+            id: selectedEl.attr("data-id"),
+            name: selectedEl.attr("data-name"),
+            lat: selectedEl.attr("data-lat"),
+            lon: selectedEl.attr("data-lon"),
+            state: selectedEl.attr("data-state"),
+            country: selectedEl.attr("data-country")
+        };
+
+        if (historyCity.id !== "") {
             setupWeatherData(historyCity);
             $("#citySearch").modal('hide');
+            $("#inputCitySearch").val("");
         }
+    });
+
+    $("#cityListSelector").on("click", function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        let chosenCity = event.target;
+        if (chosenCity.dataset.name != undefined) {
+            //save
+          $("#cityList").modal("hide");
+          $("#citySearch").modal("hide");
+          $("#inputCitySearch").val("");
+          saveSearchHistory(chosenCity.dataset);
+          setupSearchHistory();
+          setupWeatherData(chosenCity.dataset);
+        }
+    });
+
+    $("#btnClose").on("click", function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        $("#inputCitySearch").val("");
     });
 } );
